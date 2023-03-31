57769e964cd9d321a8cb5d35652b5ba2c63dd5c2
glory-99 <rongy@uchicago.edu>
2023-03-30 21:06:17 -0500

    random 50 model

---
+++ model_random50.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+class MLP_variant(nn.Module):
+    def __init__(self, N, p, num_nodes, ac_func='relu', dropout=0):
+        super(MLP_variant, self).__init__()
+        fc = []
+        last_node = N
+        for node in num_nodes:
+            fc.append(nn.Linear(last_node, node))
+            last_node = node 
+        self.fc_final = nn.Linear(last_node, p)
+        self.fc = nn.ModuleList(fc)
+        assert ac_func in ('relu', 'tanh', 'leakyrelu', 'softplus', 'softsign', 'selu', 'elu')
+        if ac_func == 'relu':
+            self.activation = nn.ReLU()
+        elif ac_func == 'tanh':
+            self.activation = nn.Tanh()
+        elif ac_func == 'leakyrelu':
+            self.activation = nn.LeakyReLU()
+        elif ac_func == 'softplus':
+            self.activation = nn.Softplus()
+        elif ac_func == 'softsign':
+            self.activation = nn.Softsign()
+        elif ac_func == 'selu':
+            self.activation = nn.SELU()
+        elif ac_func == 'elu':
+            self.activation = nn.ELU()
+        self.mseloss = nn.MSELoss()
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x):
+        for m in self.fc:
+            x = self.dropout(self.activation(m(x)))
+        x = self.fc_final(x)
+        return x 
+    
+    def get_mseloss(self, data, targ):
+        output = self.forward(data)
+        loss = self.mseloss(output, targ)
+        return loss 
+
+    def get_quanloss(self, data, targ, tau):
+        output = self.forward(data)
+        errs = targ - output 
+        loss = torch.mean(torch.max((tau-1)*errs, tau*errs))
+        return loss         
+
+    def get_quanloss_random(self, data, targ, tau):
+        output = self.forward(data)
+        errs = targ - output 
+        perm = torch.randperm(errs.size(1))
+        idx = perm[:50]
+        errs = errs[:,idx]
+        loss = torch.mean(torch.max((tau-1)*errs, tau*errs))
+        return loss
